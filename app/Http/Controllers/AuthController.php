b7f04fd1b5e475c917f30d9210ff843ea9eb4fce
adrianfnd <fernanda.adrian2@gmail.com>
2024-07-02 16:02:14 +0700

    create auth + middleware

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ], [
+            'email.required' => 'Email wajib diisi',
+            'email.email' => 'Email tidak valid',
+            'password.required' => 'Password wajib diisi',
+        ]);
+    
+        $credentials = $request->only('email', 'password');
+    
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user()->load('role');
+
+            if ($user->role->name == 'pemeliharaan') {
+                return redirect()->route('pemeliharaan.landing');
+            } elseif ($user->role->name == 'fasilitas') {
+                return redirect()->route('fasilitas.landing');
+            } elseif ($user->role->name == 'admin') {
+                return redirect()->route('admin.landing');
+            } elseif ($user->role->name == 'vendor') {
+                return redirect()->route('vendor.landing');
+            }
+        }
+    
+        return back()->withErrors(['credentials' => 'Password atau Username yang anda masukkan salah']);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect('/login');
+    }
+}
---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $role
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        $user = Auth::user()->load('role');
+
+        if ($user->role->name == $role) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized');
+    }
+}
