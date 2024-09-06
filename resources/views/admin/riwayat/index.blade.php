f9c1d630c652f5f5bbb251c5c905c2f8c9246c50
adrianfnd <fernanda.adrian2@gmail.com>
2024-09-06 11:42:26 +0700

    create function search + filter

--- resources/views/admin/riwayat/index.blade.php
+++ resources/views/admin/riwayat/index.blade.php
@@ -18,8 +18,18 @@
                         <h4 class="card-title">Daftar Riwayat Penyewaan</h4>
                         <p class="card-description">Berikut adalah daftar riwayat penyewaan yang telah selesai.</p>
 
+                        <div class="d-flex">
+                            <div class="me-2">
+                                <input type="text" id="searchInput" onkeyup="searchTable()" placeholder="Search..."
+                                    class="form-control form-control-sm">
+                            </div>
+                            <div class="select-wrapper">
+                                <select id="columnSelect" onchange="searchTable()" class="form-control form-control-sm">
+                                </select>
+                            </div>
+                        </div>
                         <div class="table-responsive">
-                            <table class="table table-striped">
+                            <table class="table table-striped" id="sewaTable">
                                 <thead>
                                     <tr>
                                         <th>No</th>
@@ -86,4 +96,58 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function generateColumnOptions() {
+            var table = document.getElementById("sewaTable");
+            var thead = table.getElementsByTagName("thead")[0];
+            var th = thead.getElementsByTagName("th");
+
+            var select = document.getElementById("columnSelect");
+
+            select.innerHTML = "";
+
+            var allOption = document.createElement("option");
+            allOption.value = "all";
+            allOption.text = "Filter";
+            select.appendChild(allOption);
+
+            for (var i = 0; i < th.length; i++) {
+                var option = document.createElement("option");
+                option.value = i;
+                option.text = th[i].innerText;
+                select.appendChild(option);
+            }
+        }
+
+        function searchTable() {
+            var input = document.getElementById("searchInput").value.toUpperCase();
+            var table = document.getElementById("sewaTable");
+            var tr = table.getElementsByTagName("tr");
+            var selectedColumn = document.getElementById("columnSelect").value;
+
+            for (var i = 1; i < tr.length; i++) {
+                var visible = false;
+                var td = tr[i].getElementsByTagName("td");
+
+                if (selectedColumn === "all") {
+                    for (var j = 0; j < td.length; j++) {
+                        if (td[j] && td[j].innerText.toUpperCase().indexOf(input) > -1) {
+                            visible = true;
+                            break;
+                        }
+                    }
+                } else {
+                    var index = parseInt(selectedColumn);
+                    if (td[index] && td[index].innerText.toUpperCase().indexOf(input) > -1) {
+                        visible = true;
+                    }
+                }
+
+                tr[i].style.display = visible ? "" : "none";
+            }
+        }
+
+        document.addEventListener("DOMContentLoaded", generateColumnOptions);
+    </script>
 @endsection
